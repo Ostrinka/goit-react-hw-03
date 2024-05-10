426e8757ffd20660dbdaf6dd9232f6e39bcb44b9
Ostrinka <148815239+Ostrinka@users.noreply.github.com>
2024-05-10 22:51:50 +0300

    step two

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,19 @@
+import SearchBox from '../SearchBox/SearchBox.jsx';
 import ContactList from '../ContactList/ContactList.jsx';
 import contacts from '../../contacts.json';
+import { useState } from 'react';
 import css from './App.module.css';
 
 export default function App() {
+  const [filter, setFilter] = useState('');
+  const visibleContacts = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
   return (
     <>
       <h1 className={css.title}>Phonebook</h1>
-      <ContactList contacts={contacts} />
+      <SearchBox filter={filter} setFilter={setFilter} />
+      <ContactList contacts={visibleContacts} />
     </>
   );
 }
