b2f211f5fba8771ac5508be0ff465776da90a33d
Ostrinka <148815239+Ostrinka@users.noreply.github.com>
2024-05-12 23:32:42 +0300

    step 3-5

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -3,14 +3,14 @@ import { FaPhone } from "react-icons/fa6";
 
 import css from './Contact.module.css'
 
-export default function Contact({ contact: { name, number } }) {
+export default function Contact({ contact: { name, number, id }, deleteContact }) {
   return (
     <div className={css.user}>
       <ul className={css.menu}>
         <li><IoPerson /> {name}</li>
         <li><FaPhone /> {number}</li>
       </ul>
-      <button className={css.btn}>Delete</button>
+      <button className={css.btn} onClick={() => deleteContact(id)}>Delete</button>
     </div>
   )
 }
